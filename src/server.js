8a7cf0a90f0ac1a1c737e7e90e8acc4ec2309b2a
vuongNguyenVg <vuongnguyen332003@gmail.com>
2024-03-23 16:03:16 +0700

    deploy-prod

--- src/server.js
+++ src/server.js
@@ -20,10 +20,17 @@ const START_SERVER = () => {
 
   //Middleware xu ly loi tap trung
   app.use(errorHandlingMiddleware)
-  app.listen(env.APP_PORT, env.APP_HOST, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Hello ${env.AUTHOR}, I am running at http://${ env.APP_HOST }:${ env.APP_PORT }/`)
-  })
+  if (env.BUILD_MODE === 'production') {
+    app.listen(process.env.PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Production: Hello ${env.AUTHOR}, I am running at Port:${process.env.PORT}/`)
+    })
+  } else {
+    app.listen(env.LOCAL_DEV_APP_PORT, env.LOCAL_DEV_APP_HOST, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Dev: Hello ${env.AUTHOR}, I am running at http://${env.LOCAL_DEV_APP_HOST}:${env.LOCAL_DEV_APP_PORT}/`)
+    })
+  }
 
   exitHook(() => {
     CLOSE_DB()
